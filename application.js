e255c41415c4dc8388cb9671df3d9e86b59ffb14
Jim Warkulwiz <jim.warkulwiz@gmail.com>
2013-07-23 18:06:07 -0400

    Revised files for 7/23

--- application.js
+++ application.js
@@ -1,3 +1,9 @@
 jQuery(document).ready(function()){
-	
+	$('#cf').on('mouseenter', function()
+		$('.bc_headings').show();
+		$('.center_headings').hide();
+	});.on('mouseleave', function() {
+		$('.center_headings').show();
+		$('.bc_headings').hide();
+	});
 });
