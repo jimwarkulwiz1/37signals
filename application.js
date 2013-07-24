ca7b264011808bc63fab3ab037dcc0e6ce11d6a2
Jim Warkulwiz <jim.warkulwiz@gmail.com>
2013-07-23 20:37:53 -0400

    For meeting

--- application.js
+++ application.js
@@ -1,8 +1,8 @@
 jQuery(document).ready(function()){
-	$('#cf').on('mouseenter', function()
+	$('#cf').on('mouseenter', function() {}
 		$('.bc_headings').show();
 		$('.center_headings').hide();
-	});.on('mouseleave', function() {
+	}).on('mouseleave', function() {
 		$('.center_headings').show();
 		$('.bc_headings').hide();
 	});
