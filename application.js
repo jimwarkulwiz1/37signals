bdaa3a47b6089587a52b1f56375d204891eec93c
Jim Warkulwiz <jim.warkulwiz@gmail.com>
2013-07-24 21:40:03 -0400

    Whatever

--- application.js
+++ application.js
@@ -1,9 +1,23 @@
-jQuery(document).ready(function()){
-	$('#cf').on('mouseenter', function() {
+jQuery(document).ready(function() {
+	$('#bc').on('mouseenter', function() {
 		$('.bc_headings').show();
 		$('.center_headings').hide();
 	}).on('mouseleave', function() {
 		$('.center_headings').show();
 		$('.bc_headings').hide();
+	});
+	$('#hr').on('mouseenter', function() {
+		$('.hr_headings').show();
+		$('.center_headings').hide();
+	}).on('mouseleave', function() {
+		$('.center_headings').show();
+		$('.hr_headings').hide();
+	});
+	$('#cf').on('mouseenter', function() {
+		$('.cf_headings').show();
+		$('.center_headings').hide();
+	}).on('mouseleave', function() {
+		$('.center_headings').show();
+		$('.cf_headings').hide();
 	});
 });
